15a641754487e5c14bdc6bc38a70c2668f5a6ada
Franklin Souza <franklintech01@protonmail.com>
2021-07-15 20:55:13 -0300

    gerador_de_senhas.py updating

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -3,20 +3,26 @@
 import random
 import os
 
-# Montando a senha
-lower = 'abcdefghijklmnopqrstuvwxyz'
-upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-numbers = '0123456789'
-symbols = '#@$%&*-=+/_'
+try:
+    # Montando a senha
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numbers = '0123456789'
+    symbols = '#@$%&*-=+/_'
 
-os.system('clear') # CLEAR
+    os.system('clear') # CLEAR
 
-# VARS_ALL
-vars_all = lower + upper + numbers + symbols
+    # VARS_ALL
+    vars_all = lower + upper + numbers + symbols
 
-# Tamanho da senha
-length = int(input('Digite o tamanho da sua senha: '))
+    # Tamanho da senha
+    length = int(input('Digite o tamanho da sua senha: '))
 
-# Gerando a senha
-password = ''.join(random.sample(vars_all , length))
-print('\n' , password)
+    # Gerando a senha
+    password = ''.join(random.sample(vars_all , length))
+    print('\n' , password)
+
+except KeyboardInterrupt:
+    os.system('clear')
+    exit()
+
